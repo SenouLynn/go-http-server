7f5c1114db95823cb33d223c3d8416d9833c80ac
Senou Lynn <77863203+SenouLynn@users.noreply.github.com>
2025-05-14 15:51:49 -0400

    Initial commit: Basic Go HTTP server setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Go HTTP Server!")
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
